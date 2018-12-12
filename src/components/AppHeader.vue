c7869d50378aa3a4f71ea5d79282d0055350f759
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-12-12 09:31:25 +0200

    connect vuex

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">
+      Image Storage
+    </a>
+    <!-- <a class="item">
+      Messages
+    </a>
+    <a class="item active">
+      Friends
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
